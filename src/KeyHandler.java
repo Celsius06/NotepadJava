850b8bc45fdf381c3c684b030c8fe12e65ba8b03
Celsius06 <minhthuan24062004@gmail.com>
2024-06-20 12:19:10 +0700

    minor updates for keyhandler

--- src/KeyHandler.java
+++ src/KeyHandler.java
@@ -18,8 +18,7 @@ public class KeyHandler implements KeyListener {
 
     // Methods
     @Override
-    public void keyTyped(KeyEvent e) {
-    }
+    public void keyTyped(KeyEvent e) {}
 
     @Override
     public void keyPressed(KeyEvent e) {
